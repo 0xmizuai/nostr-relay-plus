bade27dac07e9e737ceef858b3a2b40ec9ca4827
Germano Percossi <germano.percossi@gmail.com>
2024-06-20 00:09:06 +0100

    unit tests for Sender signers

--- nostr-crypto/src/eoa_signer.rs
+++ nostr-crypto/src/eoa_signer.rs
@@ -1,14 +1,14 @@
-use alloy_primitives::{Address, Signature};
-use anyhow::{anyhow, Result};
 use crate::signer::{Signer, Verifier};
 use crate::wallet::AlloyWallet;
+use alloy_primitives::{Address, Signature};
+use anyhow::{anyhow, Result};
 
 pub struct EoaSigner(AlloyWallet);
 
 impl EoaSigner {
     pub fn from_bytes(bytes: &[u8; 32]) -> Self {
         Self {
-            0: AlloyWallet::new(bytes)
+            0: AlloyWallet::new(bytes),
         }
     }
 
@@ -24,7 +24,7 @@ impl Signer for EoaSigner {
 }
 
 impl Verifier for EoaSigner {
-    fn verify(message: [u8; 32], signature_bytes: &[u8], pub_key: &[u8]) -> anyhow::Result<()> {
+    fn verify(message: [u8; 32], signature_bytes: &[u8], pub_key: &[u8]) -> Result<()> {
         let signature = Signature::try_from(signature_bytes)?;
         let address_recovered = AlloyWallet::ec_recover_hash(&message, &signature)?;
         let address_real = Address::try_from(pub_key)?;
@@ -32,6 +32,37 @@ impl Verifier for EoaSigner {
             Ok(())
         } else {
             Err(anyhow!("Address do not match"))
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::eoa_signer::EoaSigner;
+    use crate::hash::sha256_hash_digests;
+    use crate::signer::{Signer, Verifier};
+    use alloy_primitives::Address;
+
+    #[test]
+    fn sign_and_verify() {
+        let signer = EoaSigner::from_bytes(&[111; 32]);
+        let message_hash = sha256_hash_digests("A nice message".as_bytes());
+        let signature = signer.try_sign(&message_hash).unwrap();
+
+        // Get address and check everything is OK
+        let address = signer.address();
+        match EoaSigner::verify(message_hash, &signature[..], address.as_ref()) {
+            Ok(_) => assert!(true),
+            Err(_) => assert!(false),
+        }
+
+        // Tamper with address and check again
+        let mut addr = address.to_vec();
+        addr[3] = 134;
+        let address = Address::from_slice(&addr);
+        match EoaSigner::verify(message_hash, &signature[..], address.as_ref()) {
+            Ok(_) => assert!(false),
+            Err(_) => assert!(true),
         }
     }
 }
--- nostr-crypto/src/schnorr_signer.rs
+++ nostr-crypto/src/schnorr_signer.rs
@@ -31,3 +31,31 @@ impl Verifier for SchnorrSigner {
         Ok(verifying_key.verify(&message, &signature)?)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::hash::sha256_hash_digests;
+    use crate::schnorr_signer::SchnorrSigner;
+    use crate::signer::{Signer, Verifier};
+
+    #[test]
+    fn sign_and_verify() {
+        let signer = SchnorrSigner::from_bytes(&[199; 32]).unwrap();
+        let mut message_hash = sha256_hash_digests("Some message".as_bytes());
+        let signature = signer.try_sign(&message_hash).unwrap();
+
+        // Get public_key and verify it works
+        let pub_key = signer.private.verifying_key().to_bytes();
+        match SchnorrSigner::verify(message_hash, &signature[..], &pub_key) {
+            Ok(_) => assert!(true),
+            Err(_) => assert!(false),
+        }
+
+        // Modify message_hash and check it fails
+        message_hash[1] = 123;
+        match SchnorrSigner::verify(message_hash, &signature[..], &pub_key) {
+            Ok(_) => assert!(false),
+            Err(_) => assert!(true),
+        }
+    }
+}
