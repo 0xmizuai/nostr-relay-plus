cccec08bce34551a7c9c9b5aef002f1dfbb4adca
Germano Percossi <germano.percossi@gmail.com>
2024-06-19 23:29:45 +0100

    Add Eoa to e2e test

--- nostr-client-plus/tests/e2e.rs
+++ nostr-client-plus/tests/e2e.rs
@@ -5,14 +5,19 @@ use nostr_crypto::schnorr_signer::SchnorrSigner;
 use nostr_crypto::sender_signer::SenderSigner;
 use std::collections::HashMap;
 use std::time::Duration;
+use nostr_crypto::eoa_signer::EoaSigner;
 
 #[tokio::test]
 async fn e2e() {
-    // Generate signing key
-    let signer = SchnorrSigner::from_bytes(&[2; 32]).unwrap();
+    // Generate signing keys
+    let schnorr_signer = SchnorrSigner::from_bytes(&[2; 32]).unwrap();
+    let eoa_signer = EoaSigner::from_bytes(&[3; 32]);
 
-    let mut client = Client::new(SenderSigner::Schnorr(signer));
+    // Create clients (one signer <-> one client)
+    let mut client = Client::new(SenderSigner::Schnorr(schnorr_signer));
     client.connect("ws://127.0.0.1:3033").await.unwrap();
+    let mut client_2 = Client::new(SenderSigner::Eoa(eoa_signer));
+    client_2.connect("ws://127.0.0.1:3033").await.unwrap();
     tokio::time::sleep(Duration::from_secs(1)).await;
 
     // Prepare subscription request and send
@@ -32,10 +37,22 @@ async fn e2e() {
     client.subscribe(req).await.unwrap();
     tokio::time::sleep(Duration::from_secs(2)).await;
 
-    // Prepare event and send
+    // Prepare event for Schnorr client and send
     let event = UnsignedEvent::new(client.sender(), 0, 1, vec![], "Hello Rust".to_string());
     let event_id = event.id();
     match client.publish(event).await {
+        Ok(_) => println!("Published OK: event {}", hex::encode(event_id)),
+        Err(err) => {
+            println!("{}", err);
+            assert!(false);
+        }
+    }
+    tokio::time::sleep(Duration::from_secs(2)).await;
+
+    // Prepare event for Eoa client and send
+    let event = UnsignedEvent::new(client_2.sender(), 0, 1, vec![], "Hello Rust".to_string());
+    let event_id = event.id();
+    match client_2.publish(event).await {
         Ok(_) => println!("Published OK: event {}", hex::encode(event_id)),
         Err(err) => {
             println!("{}", err);
