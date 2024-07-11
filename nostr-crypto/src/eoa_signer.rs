c75e423255a0b4364918188d2e9a8436efe71a7d
Germano Percossi <germano.percossi@gmail.com>
2024-07-11 16:10:18 +0100

    Switch eoa to sign message and not hash

--- nostr-crypto/src/eoa_signer.rs
+++ nostr-crypto/src/eoa_signer.rs
@@ -19,14 +19,14 @@ impl EoaSigner {
 
 impl Signer for EoaSigner {
     fn try_sign(&self, message: &[u8; 32]) -> Result<Vec<u8>> {
-        Ok(self.0.sign_digest(message)?.into())
+        Ok(self.0.sign_message(message)?.into())
     }
 }
 
 impl Verifier for EoaSigner {
     fn verify(message: [u8; 32], signature_bytes: &[u8], pub_key: &[u8]) -> Result<()> {
         let signature = Signature::try_from(signature_bytes)?;
-        let address_recovered = AlloyWallet::ec_recover_hash(&message, &signature)?;
+        let address_recovered = AlloyWallet::ec_recover_message(&message, &signature)?;
         let address_real = Address::try_from(pub_key)?;
         if address_real == address_recovered {
             Ok(())
