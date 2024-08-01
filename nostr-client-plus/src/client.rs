10640cbaf1e3e3a4c12c1c8e69193bb24db83e21
Germano Percossi <germano.percossi@gmail.com>
2024-08-01 19:09:23 +0100

    Client: merge 2 tasks in one select to simplify ws management
    We need to be able to manage websocket in case of disconnections,
    so it's cleaner to have both ends in the same task.
    
    This means the internal channel must be sync (unbounded), so we
    do not wait for send in one of the two select branches, while the
    other one is supposed to be reading: this could result in a deadlock
    given writers and readers are in the same selecy statement.

--- nostr-client-plus/src/client.rs
+++ nostr-client-plus/src/client.rs
@@ -1,4 +1,5 @@
 use crate::client_command::ClientCommand;
+use crate::close::Close;
 use crate::event::UnsignedEvent;
 use crate::request::Request;
 use anyhow::{anyhow, Result};
@@ -10,22 +11,24 @@ use nostr_plus_common::relay_ok::RelayOk;
 use nostr_plus_common::sender::Sender as NostrSender;
 use nostr_plus_common::types::{Bytes32, SubscriptionId};
 use std::collections::HashMap;
-use tokio::sync::mpsc::{self, Receiver, Sender};
+use tokio::sync::mpsc::{self, Receiver, Sender, UnboundedReceiver, UnboundedSender};
 use tokio::sync::oneshot;
 use tokio::time::{timeout, Duration};
 use tokio_tungstenite::connect_async;
 use tokio_tungstenite::tungstenite::Message;
-use crate::close::Close;
 
 pub struct Client {
     signer: SenderSigner,
-    int_tx: Option<Sender<ClientCommand>>,
+    int_tx: Option<UnboundedSender<ClientCommand>>,
     // ToDo: keep track of subscriptions
 }
 
 impl Client {
     pub fn new(signer: SenderSigner) -> Self {
-        Self { signer, int_tx: None }
+        Self {
+            signer,
+            int_tx: None,
+        }
     }
 
     pub async fn connect(&mut self, url: &str) -> Result<()> {
@@ -43,8 +46,13 @@ impl Client {
             return Err(anyhow!("Already connected"));
         }
 
-        let (int_tx, mut int_rx): (Sender<ClientCommand>, Receiver<ClientCommand>) =
-            mpsc::channel(32);
+        // Warning: both sides of this channel are used in a tokio select. This needs to be unbounded because
+        // we cannot afford to wait on it in one of the two branches, otherwise we could block
+        // indefinitely
+        let (int_tx, mut int_rx): (
+            UnboundedSender<ClientCommand>,
+            UnboundedReceiver<ClientCommand>,
+        ) = mpsc::unbounded_channel();
 
         let (socket, resp) = match connect_async(url).await {
             Ok((socket, response)) => (socket, response),
@@ -54,53 +62,61 @@ impl Client {
 
         let (mut ws_write, mut ws_read) = socket.split();
 
-        let int_tx_clone = int_tx.clone();
         // Spawn listener
-        tokio::spawn(async move {
-            while let Some(msg) = ws_read.next().await {
-                match msg {
-                    Ok(Message::Text(message)) => {
-                        Self::handle_incoming_message(message, &int_tx_clone, sink.as_ref()).await
-                    }
-                    Ok(Message::Ping(_)) => {} // tungstenite handles Pong already
-                    Ok(Message::Close(c)) => println!("Server closed connection: {:?}", c), // ToDo: do something
-                    Ok(_) => println!("Unsupported message"),
-                    Err(err) => println!("Do something about {}", err), // ToDo: handle error
-                }
-            }
-            eprintln!("Error in listener while reading form channel: closing");
-        });
-
-        // Spawn sender, reading commands from internal channel
+        let int_tx_clone = int_tx.clone();
         tokio::spawn(async move {
             // Keep track of ack requests
             let mut ack_table: HashMap<Bytes32, oneshot::Sender<RelayOk>> = HashMap::new();
 
-            while let Some(message) = int_rx.recv().await {
-                match message {
-                    ClientCommand::Req(req) => {
-                        if ws_write.send(Message::from(req.to_string())).await.is_err() {
-                            eprintln!("Req: websocket error"); // ToDo: do something better
-                            break;
+            loop {
+                tokio::select! {
+                    maybe_ws_msg = ws_read.next() => {
+                        match maybe_ws_msg {
+                            Some(msg) => match msg {
+                                Ok(Message::Text(message)) => {
+                                    Self::handle_incoming_message(message, &int_tx_clone, sink.as_ref()).await
+                                }
+                                Ok(Message::Ping(_)) => {} // tungstenite handles Pong already
+                                Ok(Message::Close(c)) => println!("Server closed connection: {:?}", c), // ToDo: do something
+                                Ok(_) => println!("Unsupported message"),
+                                Err(err) => println!("Do something about {}", err), // ToDo: handle error
+                            }
+                            None => {
+                                eprintln!("Client websocket probably closed");
+                                break; // ToDo: do something
+                            }
                         }
                     }
-                    ClientCommand::Event((event, tx)) => {
-                        // Add request to ack table
-                        let _ = &ack_table.insert(event.id(), tx); // ToDo: handle existing entries
-                        if ws_write.send(Message::from(event.to_string())).await.is_err() {
-                            eprintln!("Event: websocket error"); // ToDo: do something better
-                            break;
-                        }
-                    }
-                    ClientCommand::Ack(ok_msg) => {
-                        if let Some(tx) = ack_table.remove(&ok_msg.event_id) {
-                            let _ = tx.send(ok_msg);
-                        }
-                    }
-                    ClientCommand::Close(close_msg) => {
-                        if ws_write.send(Message::from(close_msg.to_string())).await.is_err() {
-                            eprintln!("Close subscription: websocket error"); // ToDo: do something better
-                            break;
+                    maybe_int_msg = int_rx.recv() => {
+                        match maybe_int_msg {
+                            Some(message) => match message {
+                                ClientCommand::Req(req) => {
+                                    if ws_write.send(Message::from(req.to_string())).await.is_err() {
+                                        eprintln!("Req: websocket error"); // ToDo: do something better
+                                        break;
+                                    }
+                                }
+                                ClientCommand::Event((event, tx)) => {
+                                    // Add request to ack table
+                                    let _ = &ack_table.insert(event.id(), tx); // ToDo: handle existing entries
+                                    if ws_write.send(Message::from(event.to_string())).await.is_err() {
+                                        eprintln!("Event: websocket error"); // ToDo: do something better
+                                        break;
+                                    }
+                                }
+                                ClientCommand::Ack(ok_msg) => {
+                                    if let Some(tx) = ack_table.remove(&ok_msg.event_id) {
+                                        let _ = tx.send(ok_msg);
+                                    }
+                                }
+                                ClientCommand::Close(close_msg) => {
+                                    if ws_write.send(Message::from(close_msg.to_string())).await.is_err() {
+                                        eprintln!("Close subscription: websocket error"); // ToDo: do something better
+                                        break;
+                                    }
+                                }
+                            }
+                        None => eprintln!("Client unrecoverable error: broken internal channel"), // toDo: try to recover
                         }
                     }
                 }
@@ -114,25 +130,23 @@ impl Client {
 
     async fn handle_incoming_message(
         msg: String,
-        tx: &Sender<ClientCommand>,
+        tx: &UnboundedSender<ClientCommand>,
         sink: Option<&Sender<RelayMessage>>,
     ) {
         match serde_json::from_str::<RelayMessage>(&msg) {
             Ok(incoming) => match incoming {
                 RelayMessage::Ok(ok_msg) => {
                     println!("ACK: {}", serde_json::to_string(&ok_msg).unwrap());
-                    let _ = tx.send(ClientCommand::Ack(ok_msg)).await;
+                    let _ = tx.send(ClientCommand::Ack(ok_msg));
                 }
-                relay_msg => {
-                    match sink {
-                        None => println!("Unhandled: {:?}", msg),
-                        Some(sender) => {
-                            if let Err(e) = sender.send(relay_msg).await {
-                                eprintln!("{}", e);
-                            }
+                relay_msg => match sink {
+                    None => println!("Unhandled: {:?}", msg),
+                    Some(sender) => {
+                        if let Err(e) = sender.send(relay_msg).await {
+                            eprintln!("{}", e);
                         }
                     }
-                }
+                },
             },
             Err(err) => eprintln!("ERROR ({}) with msg: {}", err, msg),
         }
@@ -146,7 +160,7 @@ impl Client {
                 // Prepare ACK channel
                 let (tx, rx) = oneshot::channel::<RelayOk>();
 
-                int_tx.send(ClientCommand::Event((event, tx))).await?;
+                int_tx.send(ClientCommand::Event((event, tx)))?;
 
                 // Wait for ACK with timeout
                 match timeout(Duration::from_secs(20), rx).await {
@@ -184,7 +198,7 @@ impl Client {
     pub async fn subscribe(&self, req: Request) -> Result<()> {
         match &self.int_tx {
             Some(sender) => {
-                sender.send(ClientCommand::Req(req)).await?;
+                sender.send(ClientCommand::Req(req))?;
             }
             None => return Err(anyhow!("Subscribe: missing websocket")),
         }
@@ -195,7 +209,7 @@ impl Client {
         let close_msg = Close::new(sub_id);
         match &self.int_tx {
             Some(sender) => {
-                sender.send(ClientCommand::Close(close_msg)).await?;
+                sender.send(ClientCommand::Close(close_msg))?;
             }
             None => return Err(anyhow!("Close subscription: missing websocket")),
         }
