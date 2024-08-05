62afca59257740ee66894b481de299357448d5a0
Germano Percossi <germano.percossi@gmail.com>
2024-08-05 08:11:19 +0100

    Revert "Decrease log level for services to INFO"
    This reverts commit e873f499912a2c63322340b27e90bdf4eae30abb.

--- nostr-client-plus/src/bin/aggregator.rs
+++ nostr-client-plus/src/bin/aggregator.rs
@@ -62,7 +62,7 @@ async fn run() -> Result<()> {
 
     // Logger setup
     let subscriber = FmtSubscriber::builder()
-        .with_max_level(tracing::Level::INFO)
+        .with_max_level(tracing::Level::DEBUG)
         .finish();
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 
